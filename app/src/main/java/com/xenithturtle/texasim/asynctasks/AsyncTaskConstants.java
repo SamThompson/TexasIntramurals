dfc7e2d370ac1ed8a04f722faded74607a208b32
st028 <sam.thompson028@utexas.edu>
2014-07-17 12:08:08 -0500

    qustom dialog added

--- app/src/main/java/com/xenithturtle/texasim/asynctasks/AsyncTaskConstants.java
+++ app/src/main/java/com/xenithturtle/texasim/asynctasks/AsyncTaskConstants.java
@@ -5,15 +5,18 @@ package com.xenithturtle.texasim.asynctasks;
  */
 public class AsyncTaskConstants {
 
+    public static final String SERVER_BASE = "http://www.cs.utexas.edu/~st028/cgi-bin/";
+
+    public static final String SERVER_STATUS = SERVER_BASE + "server_status.scgi";
+
     public static final String IM_REQ_BASE =
-            "http://www.cs.utexas.edu/~st028/cgi-bin/im_data_request.scgi?";
+            SERVER_BASE + "im_data_request.scgi?";
 
     public static final String LEAGUES_REQ_BASE =
-            "http://www.cs.utexas.edu/~st028/cgi-bin/league_data_request.scgi?event=";
+            SERVER_BASE + "league_data_request.scgi?event=";
+
     public static final String LEAGUES_REQ_MID = "&request=";
 
-    public static final String RECSPORTS_BASE =
-            "http://www.utrecsports.org/intramurals/schedules-and-scores?type=sheet&lid=";
 
     //JSON constants
     public static final String EID = "event_id";
@@ -24,6 +27,8 @@ public class AsyncTaskConstants {
     public static final String LNAME = "league_name";
     public static final String LINFO = "league_info";
     public static final String LEAGUES = "leagues";
+    public static final String STATUS = "status_ok";
+    public static final String MESSAGE = "message";
 
     //Deals with league requests
     public static final String NAME = "name";
--- app/src/main/java/com/xenithturtle/texasim/fragments/MyLeaguesFragment.java
+++ app/src/main/java/com/xenithturtle/texasim/fragments/MyLeaguesFragment.java
@@ -1,18 +1,35 @@
 package com.xenithturtle.texasim.fragments;
 
 import android.app.Activity;
-import android.net.Uri;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 
+import com.qustom.dialog.QustomDialogBuilder;
 import com.xenithturtle.texasim.adapters.IMSqliteAdapter;
+import com.xenithturtle.texasim.asynctasks.AsyncTaskConstants;
 import com.xenithturtle.texasim.cards.LeagueCard;
 import com.xenithturtle.texasim.R;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,6 +45,11 @@ import it.gmariotti.cardslib.library.view.CardListView;
 public class MyLeaguesFragment extends Fragment {
 
     private OnFollowButtonPressedListener mListener;
+    private boolean mStatusOk = false;
+    private ProgressBar mProgressBar;
+    private LinearLayout mNoLeaguesLayout;
+    private LinearLayout mErrorLayout;
+    private CardListView mCardListView;
 
     public static MyLeaguesFragment newInstance() {
         MyLeaguesFragment fragment = new MyLeaguesFragment();
@@ -41,6 +63,7 @@ public class MyLeaguesFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        new ServerAsyncTask().execute();
     }
 
     @Override
@@ -48,36 +71,30 @@ public class MyLeaguesFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v =  inflater.inflate(R.layout.fragment_my_leagues, container, false);
+        mProgressBar = (ProgressBar) v.findViewById(R.id.progress_bar);
+        mNoLeaguesLayout = (LinearLayout) v.findViewById(R.id.no_leagues);
+        mErrorLayout = (LinearLayout) v.findViewById(R.id.error_layout);
+        mCardListView = (CardListView) v.findViewById(R.id.cardlist);
 
-
-        //TODO: for now do this here, but move cardCreation to oncreate later
-        IMSqliteAdapter sqliteAdapter = new IMSqliteAdapter(getActivity());
-        sqliteAdapter.open();
-        ArrayList<Card> cards = new ArrayList<Card>();
-        //add cards here
-        List<Integer> followingLeagues = sqliteAdapter.getFollowingLeagues();
-        if (followingLeagues.size() > 0) {
-            for (Integer i : followingLeagues) {
-                LeagueCard l = new LeagueCard(getActivity(), i);
-                cards.add(l);
+        //set follow button onclick
+        (v.findViewById(R.id.follow_button)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onFollowButtonPressed();
             }
-            sqliteAdapter.close();
+        });
 
-            CardArrayAdapter ca = new CardArrayAdapter(getActivity(), cards);
-            CardListView list = (CardListView) v.findViewById(R.id.cardlist);
-            list.setAdapter(ca);
-            (v.findViewById(R.id.progress_bar)).setVisibility(View.GONE);
-            list.setVisibility(View.VISIBLE);
-        } else {
-            (v.findViewById(R.id.follow_button)).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    mListener.onFollowButtonPressed();
-                }
-            });
-            (v.findViewById(R.id.progress_bar)).setVisibility(View.GONE);
-            (v.findViewById(R.id.no_leagues)).setVisibility(View.VISIBLE);
-        }
+        //set try again onclick
+        (v.findViewById(R.id.try_again)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCardListView.setVisibility(View.GONE);
+                mNoLeaguesLayout.setVisibility(View.GONE);
+                mErrorLayout.setVisibility(View.GONE);
+                mProgressBar.setVisibility(View.VISIBLE);
+                new ServerAsyncTask().execute();
+            }
+        });
 
         return v;
     }
@@ -99,6 +116,54 @@ public class MyLeaguesFragment extends Fragment {
         mListener = null;
     }
 
+    public void setContentView() {
+
+        //TODO: for now do this here, but move cardCreation to oncreate later
+        IMSqliteAdapter sqliteAdapter = new IMSqliteAdapter(getActivity());
+        sqliteAdapter.open();
+        ArrayList<Card> cards = new ArrayList<Card>();
+        //add cards here
+        List<Integer> followingLeagues = sqliteAdapter.getFollowingLeagues();
+        if (followingLeagues.size() > 0) {
+            for (Integer i : followingLeagues) {
+                LeagueCard l = new LeagueCard(getActivity(), i);
+                cards.add(l);
+            }
+            sqliteAdapter.close();
+
+            CardArrayAdapter ca = new CardArrayAdapter(getActivity(), cards);
+            mCardListView.setAdapter(ca);
+            mProgressBar.setVisibility(View.GONE);
+            mCardListView.setVisibility(View.VISIBLE);
+        } else {
+            mProgressBar.setVisibility(View.GONE);
+            mNoLeaguesLayout.setVisibility(View.VISIBLE);
+        }
+
+    }
+
+    public void setErrorView(Pair<Boolean, String> res) {
+        mProgressBar.setVisibility(View.GONE);
+        mCardListView.setVisibility(View.GONE);
+        mNoLeaguesLayout.setVisibility(View.GONE);
+        mErrorLayout.setVisibility(View.VISIBLE);
+
+        if (res != null && !res.first) {
+            QustomDialogBuilder builder = new QustomDialogBuilder(getActivity());
+            builder.setTitle("Message from server");
+            builder.setMessage(res.second + "\n");
+            builder.setDividerColorResource(R.color.card_orange);
+            builder.setTitleColorResource(android.R.color.black);
+            builder.setNegativeButton("Close" , new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                }
+            });
+            builder.show();
+        }
+    }
+
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
@@ -112,6 +177,56 @@ public class MyLeaguesFragment extends Fragment {
     public interface OnFollowButtonPressedListener {
         // TODO: Update argument type and name
         public void onFollowButtonPressed();
+    }
+
+
+    public class ServerAsyncTask extends AsyncTask<Void, Void, Pair<Boolean, String>> {
+
+        @Override
+        public void onPreExecute() {
+        }
+
+        @Override
+        public Pair<Boolean, String> doInBackground(Void ... params) {
+            HttpClient client = new DefaultHttpClient();
+
+            HttpGet get = new HttpGet(AsyncTaskConstants.SERVER_STATUS);
+            String response;
+            try {
+                response = client.execute(get, new BasicResponseHandler());
+            } catch (UnsupportedEncodingException e) {
+                Log.e("*********", e.toString());
+                return null;
+            } catch (IOException e) {
+                Log.e("*********", e.toString());
+                return null;
+            }
+
+            JSONObject res;
+            try {
+                res = new JSONObject(response);
+                boolean status = res.getBoolean(AsyncTaskConstants.STATUS);
+                String message = res.getString(AsyncTaskConstants.MESSAGE);
+                return new Pair<Boolean, String>(status, message);
+            } catch (JSONException e) {
+                Log.e("*********", e.toString());
+                return null;
+            }
+
+        }
+
+        @Override
+        public void onPostExecute(Pair<Boolean, String> res) {
+            if (res != null) {
+                if (res.first) {
+                    setContentView();
+                } else {
+                    setErrorView(res);
+                }
+            } else {
+                setErrorView(res);
+            }
+        }
     }
 
 }
